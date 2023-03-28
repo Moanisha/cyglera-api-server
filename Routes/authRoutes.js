5a43900012ce364598ca0c5014cd24762060709d
Sethu Madhava Naidu Lankipalli <sethumadhav202@gmail.com>
2023-03-27 23:16:55 -0400

    dashboard

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -5,11 +5,13 @@ const SignupController = require("../controllers/SignupController");
 const ProfileController = require("../Controllers/ProfileController");
 const FetchProfileController = require("../Controllers/FetchController");
 const fetchAllRecipesController = require("../Controllers/fetchAllRecipesController")
+const DashboardController = require('../Controllers/DashboardController');
 
 router.post("/signup", SignupController);
 router.post("/signin", SigninController);
 router.post("/profile", ProfileController);
 router.post("/fetchprofile", FetchProfileController);
 router.get("/fetchAllRecipes", fetchAllRecipesController);
+router.post("/getDashboard", DashboardController );
 
 module.exports = router;
